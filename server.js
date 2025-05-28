00a9f76a496258f7f96ecd4113f52f550a2c0005
Milan Bhandari <108926680+milancodess@users.noreply.github.com>
2025-05-28 13:35:02 +0545

    Update server.js

--- server.js
+++ server.js
@@ -29,26 +29,26 @@ app.get('/search', async (req, res) => {
     const results = [];
 
     $('.ipc-metadata-list-summary-item').each((i, element) => {
-  const title = $(element).find('.ipc-metadata-list-summary-item__t').text().trim();
-  const listItems = $(element).find('.ipc-metadata-list-summary-item__li');
+      const title = $(element).find('.ipc-metadata-list-summary-item__t').text().trim();
+      const listItems = $(element).find('.ipc-metadata-list-summary-item__li');
 
-  let year = '';
-  let type = 'Unknown';
+      let year = '';
+      let type = 'Movie';
 
-  listItems.each((i, el) => {
-    const text = $(el).text().trim();
-    if (/^\d{4}/.test(text) && !year) {
-      year = text;
-    } else if (/(TV Series|TV Movie|Video|Short|Movie|Documentary|Mini-Series|Podcast|Game)/i.test(text)) {
-      type = text;
-    }
-  });
+      listItems.each((i, el) => {
+        const text = $(el).text().trim();
+        if (/^\d{4}/.test(text) && !year) {
+          year = text;
+        } else if (/(TV Series|TV Movie|Video|Short|Movie|Documentary|Mini-Series|Podcast|Game)/i.test(text)) {
+          type = text;
+        }
+     });
 
   const href = $(element).find('a').attr('href');
   const idMatch = href?.match(/title\/tt(\d+)\//);
   const poster = $(element).find('.ipc-image').attr('src');
 
-  if (title && idMatch) {
+  if (title && idMatch && year && type !== 'Unknown' && poster) {
     results.push({
       id: idMatch[1],
       title,
